026b62f471ac9041a58e62fe0d07e404c1ce8049
datkanber <burak2kanber@gmail.com>
2024-12-28 22:12:46 +0300

    v1.02

---
+++ SimpleShoppingList/SimpleShoppingList/Migrations/Configuration.cs
@@ -0,0 +1,35 @@
+﻿namespace SimpleShoppingList.Migrations
+{
+    using SimpleShoppingList.Controllers;
+    using SimpleShoppingList.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SimpleShoppingList.Models.SimpleShoppingListContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SimpleShoppingList.Models.SimpleShoppingListContext context)
+        {
+            context.ShoppingLists.AddOrUpdate(
+                new ShoppingList {
+                    Name = "Groceries",
+                    Items = {
+                        new Item { Name = "Milk" },
+                        new Item { Name = "Cornflakes" },
+                        new Item { Name = "Strawberries" }
+                    }
+                },
+                new ShoppingList
+                {
+                    Name = "Hardware"
+                }
+                );
+        }
+    }
+}
--- SimpleShoppingList/SimpleShoppingList/Scripts/app.js
+++ SimpleShoppingList/SimpleShoppingList/Scripts/app.js
@@ -1,13 +1,21 @@
 ﻿var currentList = {};
 
-
 function createShoppingList() {
     currentList.name = $("#shoppingListName").val();
     currentList.items = new Array();
 
-    // web service call
-
-    showShoppingList();
+    // Web Service Call
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "api/ShoppingListsEF/",
+        data: currentList,
+        success: function (result) {
+            currentList = result;
+            showShoppingList();
+            history.pushState({ id: result.id }, result.name, "?id=" + result.id);
+        }
+    });
 }
 
 function showShoppingList() {
@@ -17,10 +25,12 @@ function showShoppingList() {
     $("#createListDiv").hide();
     $("#shoppingListDiv").show();
 
-    $("shoppingListName").focus();
-    $("shoppingListName").keyup(function (event) {
+    $("#newItemName").val("");
+    $("#newItemName").focus();
+    $("#newItemName").unbind("keyup");
+    $("#newItemName").keyup(function (event) {
         if (event.keyCode == 13) {
-            createShoppingList();
+            addItem();
         }
     });
 }
@@ -28,11 +38,19 @@ function showShoppingList() {
 function addItem() {
     var newItem = {};
     newItem.name = $("#newItemName").val();
-    currentList.items.push(newItem);
-    console.info(currentList);
+    newItem.shoppingListId = currentList.id;
 
-    drawItems();
-    $("#newItemName").val("");
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "api/ItemsEF/",
+        data: newItem,
+        success: function (result) {
+            currentList = result;
+            drawItems();
+            $("#newItemName").val("");
+        }
+    });
 }
 
 function drawItems() {
@@ -43,54 +61,97 @@ function drawItems() {
         var $li = $("<li>").html(currentItem.name)
             .attr("id", "item_" + i);
         var $deleteBtn =
-            $("<button onclick='deleteItem(" + i + ")'>Delete</button>").appendTo($li);
+            $("<button onclick='deleteItem(" + currentItem.id + ")'>D</button>").appendTo($li);
         var $checkBtn =
-            $("<button onclick='checkItem(" + i + ")'>Check</button>").appendTo($li);
+            $("<button onclick='checkItem(" + currentItem.id + ")'>C</button>").appendTo($li);
+
+        if (currentItem.checked) {
+            $li.addClass("checked");
+        }
 
         $li.appendTo($list);
     }
 }
 
-function deleteItem(index) {
-    currentList.items.splice(index, 1);
-    drawItems();
+function deleteItem(itemId) {
+    $.ajax({
+        type: "DELETE",
+        dataType: "json",
+        url: "api/ItemsEF/" + itemId,
+        success: function (result) {
+            currentList = result;
+            drawItems();
+        }
+    });
 }
 
-function checkItem(index) {
-    if ($("#item_" + index).hasClass("checked")) {
-        $("#item_" + index).removeClass("checked");
+function checkItem(itemId) {
+    var changedItem = {};
+
+    for (var i = 0; i < currentList.items.length; i++) {
+        if (currentList.items[i].id == itemId) {
+            changedItem = currentList.items[i];
+        }
     }
-    else { 
-        $("#item_" + index).addClass("checked");
-    }
+
+    changedItem.checked = !changedItem.checked;
+
+    $.ajax({
+        type: "PUT",
+        dataType: "json",
+        url: "api/ItemsEF/" + itemId,
+        data: changedItem,
+        success: function (result) {
+            changedItem = result;
+            drawItems();
+        }
+    });
 }
 
 function getShoppingListById(id) {
-    console.info(id);
-
-    currentList.name = "Mock Shopping List";
-    currentList.items = [
-        { name: "Milk" },
-        { name: "Bread" },
-        { name: "Strawberriees" }
-    ];
-
-    showShoppingList();
-    drawItems();
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "api/ShoppingListsEF/" + id,
+        success: function (result) {
+            currentList = result;
+            showShoppingList();
+            drawItems();
+        }
+    });
 }
-$(document).ready(function () {
-    console.info("ready");
+
+function hideShoppingList() {
+    $("#createListDiv").show();
+    $("#shoppingListDiv").hide();
+
+    $("#shoppingListName").val("");
     $("#shoppingListName").focus();
-    $("shoppingListName").keyup(function (event) {
+    $("#shoppingListName").unbind("keyup");
+    $("#shoppingListName").keyup(function (event) {
         if (event.keyCode == 13) {
             createShoppingList();
         }
     });
+}
+
+$(document).ready(function () {
+    console.info("ready");
+
+    hideShoppingList();
 
     var pageUrl = window.location.href;
     var idIndex = pageUrl.indexOf("?id=");
     if (idIndex != -1) {
         getShoppingListById(pageUrl.substring(idIndex + 4));
     }
-});
 
+    window.onpopstate = function (event) {
+        if (event.state == null) {
+            hideShoppingList();
+        }
+        else {
+            getShoppingListById(event.state.id);
+        }
+    };
+});
